da058d86116b95eb5ba77f09ea4be9000c75b217
burgi <yaron.burg@gmail.com>
2014-11-30 15:41:04 +0200

    open linked nodes on click

--- server.js
+++ server.js
@@ -57,41 +57,48 @@ app.get('/query', function(request, response) {
 });
 
 app.get('/expand', function(request, response) {
-    var queryString = request.query.q;
-    db.query('MATCH node RETURN node', null, function(err, results) {
-      if (err) throw err;
-      var nodes = [], edges = [];
+  var id = request.query.id;
+  var query = "START a = node(" + id + ") MATCH (a)-[r]-(b) RETURN b";
+  console.log(query);
+  db.query(query, null, function(err, results) {
+    if (err) throw err;
+    var nodes = [], edges = [];
+    var ids=[];
+    ids.push(id);
+    results.map(function (result) {
+      var node = {};
+      var id;
+      var label;
+      var name;
+      id = result.b._data.metadata.id;
+      if (result.b._data.metadata.labels.length > 0) {
+        label = result.b._data.metadata.labels[0];
+      } else {
+        label = "Node";
+      }
+      if (result.b._data.data.Name) {
+        name = result.b._data.data.Name;
+      } else {
+        name = label;
+      }
+      node.name = name;
+      node.label = label;
+      node.id = id;
+      node.properties = result.b._data.data;
+      nodes.push(node);
+      ids.push(id);
+    });
+    query =  "START a = node(" + ids + "), b = node("+ ids + ") MATCH a -[r]-> b RETURN r"; 
+    console.log(query);
+    db.query(query, null, function(err, results) {
       results.map(function (result) {
-        var node = {};
-        var id;
-        var label;
-        var name;
-        id = result.node._data.metadata.id;
-        if (result.node._data.metadata.labels.length > 0) {
-          label = result.node._data.metadata.labels[0];
-        } else {
-          label = "Node";
-        }
-        if (result.node._data.data.Name) {
-          name = result.node._data.data.Name;
-        } else {
-          name = label;
-        }
-        node.name = name;
-        node.label = label;
-        node.id = id;
-        node.properties = result.node._data.data;
-        nodes.push(node);
-      });     
-      db.query('MATCH (node)-[rel]-() RETURN rel', null, function(err, results) {
-        results.map(function (result) {
-          var url = result.rel.db.url + "/db/data/node/";
-          var edge = {"source": parseInt(result.rel._data.start.replace(url, "")),
-            "target": parseInt(result.rel._data.end.replace(url, "")),
-            "value": result.rel._data.type,
-            "id": result.rel._data.metadata.id            
-          };
-          edges.push(edge);
+        var url = result.r.db.url + "/db/data/node/";
+        var edge = {"source": parseInt(result.r._data.start.replace(url, "")),
+          "target": parseInt(result.r._data.end.replace(url, "")),
+          "value": result.r._data.type,
+          "id": result.r._data.metadata.id            
+        };
+        edges.push(edge);
       });
       var obj = {"nodes": nodes, "links": edges}
       response.send(JSON.stringify(obj));
