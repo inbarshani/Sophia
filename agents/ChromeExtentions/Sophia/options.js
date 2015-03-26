2f24d4a9d1360b89780e61e98aaef4f7fafdb464
burgi <yaron.burg@gmail.com>
2015-03-26 10:49:17 +0200

    add data URL option to extension

--- agents/ChromeExtentions/Sophia/options.js
+++ agents/ChromeExtentions/Sophia/options.js
@@ -1,17 +1,29 @@
 window.addEventListener("load", function() {
-    var baseUrl;
-    chrome.storage.local.get('baseUrl', function (result) {
-        baseUrl = result.baseUrl;
-        var urlText = document.getElementById("url");
-        if (baseUrl == undefined) {
-            baseUrl = "localhost";
+    var baseAppUrl, dataUrl;
+    chrome.storage.local.get('baseAppUrl', function (result) {
+        baseAppUrl = result.baseAppUrl;
+        var appUrlText = document.getElementById("appUrl");
+        if (baseAppUrl == undefined) {
+            baseAppUrl = "localhost";
         }
-        urlText.value = baseUrl;
+        appUrlText.value = baseAppUrl;
+    });
+    chrome.storage.local.get('dataUrl', function (result) {
+        dataUrl = result.dataUrl;
+        var dataUrlText = document.getElementById("dataUrl");
+        if (dataUrl == undefined) {
+            dataUrl = "localhost:8080/data";
+        }
+        dataUrlText.value = dataUrl;
     });
     document.getElementById("saveButton").addEventListener("click",function() {
-        var urlText = document.getElementById("url");
-        chrome.storage.local.set({'baseUrl': urlText.value}, function() {
-          console.log('Settings saved');
+        var appUrlText = document.getElementById("appUrl");
+        var dataUrlText = document.getElementById("dataUrl");
+        chrome.storage.local.set({'baseAppUrl': appUrlText.value}, function() {
+          console.log('App URL Settings saved');
+        });
+        chrome.storage.local.set({'dataUrl': dataUrlText.value}, function() {
+          console.log('Data URL Settings saved');
         });
     });
 });
