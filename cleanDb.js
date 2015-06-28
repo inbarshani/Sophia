de43183f020b4fbfce2c67516d5a2019ed43e383
unknown <shanii@SHANII4.americas.hpqcorp.net>
2015-06-28 12:06:09 +0300

    clean up repo - remove the mockup files

--- cleanDb.js
+++
@@ -1,8 +0,0 @@
-var neo4j = require("neo4j");
-// connect to neo4j DB
-var db = new neo4j.GraphDatabase('http://localhost:7474');
-// clean DB
-db.query('MATCH (n) OPTIONAL MATCH (n)-[m]-() DELETE n, m', null, function(err, results) {
-    console.log('DB clean');
-    if (err) console.error('neo4j query to delete all failed: ' + err + '\n');
-});
