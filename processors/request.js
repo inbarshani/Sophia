e28a6faf30d2ed817f7cf472914784c7e5e99590
Inbar Shani <inbar.shani@hp.com>
2015-09-03 15:21:13 +0300

    fix format of jetty logs

--- processors/request.js
+++ processors/request.js
@@ -3,7 +3,7 @@ var dateTime = require('../lib/dateTime');
 module.exports = {
 	getData: function (obj) {
 		var isUTC = true;
-		var d = dateTime.getDateFromFormat(obj.timestamp, 'dd/MMM/yyyy:hh:mm:ss', isUTC);;
+		var d = dateTime.getDateFromFormat(obj.timestamp, 'dd/MMM/yyyy:hh:mm:ss.SSS', isUTC);;
         obj.timestamp = d.getTime();
         obj.high_priority_index = obj.message.substring(obj.message.indexOf(']'));
         obj.indexable_content = obj.message + ' ' + obj.host;
