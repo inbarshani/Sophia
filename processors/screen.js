efae26cead87d8cb7d17d3af5d78ecb4420b897c
Inbar Shani <inbar.shani@hp.com>
2015-10-07 14:41:53 +0300

    correctly encode and decode object values

--- processors/screen.js
+++ processors/screen.js
@@ -16,6 +16,7 @@ module.exports = {
         {
             formatted_result.date = idol_document['DREDATE'][0];
             formatted_result.timestamp = idol_document['TIMESTAMP'][0];
+            formatted_result.phash = decodeURIComponent(idol_document['PHASH'][0]);
         //    formatted_result.action = idol_document['ACTION'][0];
          //   formatted_result.status = idol_document['STATUS'][0];
         }
