bf0904f0bd9d8a9c2b81d6d728c427cac40891fd
gazitn <nitzan.gazit@hp.com>
2015-10-07 15:59:39 +0300

    Added more data to request Idole

--- processors/request.js
+++ processors/request.js
@@ -12,6 +12,14 @@ module.exports = {
 
 	extractDataFromIDOL: function(idol_document, formatted_result,isExpendedData){
 		formatted_result.caption = idol_document['MESSAGE'][0];
+        if(isExpendedData)
+        {
+            formatted_result.date = idol_document['DREDATE'][0];
+            formatted_result.timestamp = idol_document['TIMESTAMP'][0];
+            //  formatted_result.phash = JSON.parse(decodeURIComponent(idol_document['PHASH'][0]));
+            //    formatted_result.action = idol_document['ACTION'][0];
+            //   formatted_result.status = idol_document['STATUS'][0];
+        }
 		return formatted_result;		
 	}
 };
--- processors/screen.js
+++ processors/screen.js
@@ -18,7 +18,7 @@ module.exports = {
         {
             formatted_result.date = idol_document['DREDATE'][0];
             formatted_result.timestamp = idol_document['TIMESTAMP'][0];
-            formatted_result.phash = JSON.parse(decodeURIComponent(idol_document['PHASH'][0]));
+          //  formatted_result.phash = JSON.parse(decodeURIComponent(idol_document['PHASH'][0]));
         //    formatted_result.action = idol_document['ACTION'][0];
          //   formatted_result.status = idol_document['STATUS'][0];
         }
