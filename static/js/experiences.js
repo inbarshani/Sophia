b14d477f0ddbee01a5d57b75e31290bd58e6d9df
Inbar Shani <inbar.shani@hp.com>
2015-10-20 12:35:08 +0300

    experiences UI

--- static/js/experiences.js
+++ static/js/experiences.js
@@ -1,15 +1,57 @@
+var quickEditConfig = {
+    space: false,
+    showbtn: false,
+    checkold: true,
+    submit: function(element, newValue) {
+        // checkold doesn't work, do it myself
+        if (element.text() == newValue)
+            return true;
+        // create a new line if this is the last item
+        // do this before changing the DOM, as the last line
+        //	is in the right format                
+        if ($(element).closest("tr").is(":last-child")) {
+            var tr = '<tr>' +
+                '<td>+</td>' +
+                '<td class="quickedit draft">Click to edit</td>' +
+                '</tr>';
+            $('#xp_steps tbody').append(tr);
+            $('#xp_steps tr:last .quickedit').quickEdit(quickEditConfig);
+        }
+        // set value and add step number
+        element.text(newValue);
+        element.removeClass('draft');
+        $(element).prev().text($('#xp_steps tr').length - 3);
+    }
+};
 
-function loadXP(){
-    $("#application_area").load("html/experiences.html", function () {
+
+function loadXP() {
+    $("#application_area").load("html/experiences.html", function() {
         // bind the search control
-
+        $('#xp_steps .quickedit').quickEdit(quickEditConfig);
+        $('#xp_flow_name').quickEdit({
+            space: false,
+            showbtn: false,
+            checkold: true,
+            autosubmit: true,
+			submit: function(element, newValue) {
+			        // checkold doesn't work, do it myself
+			        if (element.text() == newValue)
+			            return true;
+			        // set value and add step number
+			        element.text(newValue);
+			        element.removeClass('draft');
+			    }            
+        });
+        $('#xp_clear').on('click',function(){
+        	clearXP();
+        })
     });
 }
 
-function defineXPDone(){
-}
+function defineXPDone() {}
 
-function searchXP(){
+function searchXP() {
     //reportString = 'Type: SAVED_TESTS\n';
     clearXP();
     //  + "&dateCondition=" + JSON.stringify(dateCondition)
@@ -33,6 +75,13 @@ function searchXP(){
     */
 }
 
-function clearXP()
-{
+function clearXP() {
+	var tr = '<tr>' +
+	    '<td>+</td>' +
+	    '<td class="quickedit draft">Click to edit</td>' +
+	    '</tr>';
+	$('#xp_steps tbody').empty().append(tr);
+	$('#xp_flow_name').text('Click to edit name');
+	$('#xp_flow_name').addClass('draft');
+    $('#xp_steps .quickedit').quickEdit(quickEditConfig);
 }
