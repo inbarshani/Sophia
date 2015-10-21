10c683f3dad6621b8f8a40b335f4f285491419e6
Inbar Shani <inbar.shani@hp.com>
2015-10-21 10:16:13 +0300

    replace quickedit with editable

--- static/js/experiences.js
+++ static/js/experiences.js
@@ -1,55 +1,91 @@
-var quickEditConfig = {
-    space: false,
-    showbtn: false,
-    checkold: true,
-    submit: function(element, newValue) {
-        // checkold doesn't work, do it myself
-        if (element.text() == newValue)
-            return true;
-        // create a new line if this is the last item
-        // do this before changing the DOM, as the last line
-        //	is in the right format                
-        if ($(element).closest("tr").is(":last-child")) {
-            var tr = '<tr>' +
-                '<td>+</td>' +
-                '<td class="quickedit draft">Click to edit</td>' +
-                '</tr>';
-            $('#xp_steps tbody').append(tr);
-            $('#xp_steps tr:last .quickedit').quickEdit(quickEditConfig);
-        }
-        // set value and add step number
-        element.text(newValue);
-        element.removeClass('draft');
-        $(element).prev().text($('#xp_steps tr').length - 3);
-    }
+var editableOptions = {
+    type: 'text',
+    mode: 'inline',
+    send: 'never',
+    showbuttons: false,
+    unsavedclass: null,
+    onblur: 'submit'
 };
+var xpStepsArray = [];
+var xpName = '';
 
 
 function loadXP() {
     $("#application_area").load("html/experiences.html", function() {
         // bind the search control
-        $('#xp_steps .quickedit').quickEdit(quickEditConfig);
-        $('#xp_flow_name').quickEdit({
-            space: false,
-            showbtn: false,
-            checkold: true,
-            autosubmit: true,
-			submit: function(element, newValue) {
-			        // checkold doesn't work, do it myself
-			        if (element.text() == newValue)
-			            return true;
-			        // set value and add step number
-			        element.text(newValue);
-			        element.removeClass('draft');
-			    }            
-        });
-        $('#xp_clear').on('click',function(){
+        $('#xp_steps .editable')
+        	.editable(editableOptions)
+        	.on('save', stepSave)
+        	.on('hidden', stepNext);
+        $('#xp_flow_name').editable(editableOptions)
+        	.on('save',function(e, params){
+		        // only act if new value is diff than current
+		        var element = $(e.target);
+		        if (element.text() == params.newValue)
+		            return true;
+		        // set value and add step number
+		        element.text(params.newValue);
+		        element.removeClass('draft');
+        	});
+ 		$('#xp_clear').on('click',function(){
         	clearXP();
+        })
+        $('#xp_create').on('click',function(){
+        	defineXPDone();
         })
     });
 }
 
-function defineXPDone() {}
+function stepNext(e, reason)
+{
+	if(reason === 'save' || reason === 'nochange') {
+	    var $next = $(this).closest('tr').next().find('.editable');
+	    setTimeout(function() {
+	        $next.editable('show');
+	    }, 300); 
+	}	
+}
+
+function stepSave(e, params)
+{
+	// only react if new value is different than existing
+	var element = $(e.target);
+	if (element.text() == params.newValue)
+	    return;
+	// create a new line if this is the last item
+	// do this before changing the DOM, as the last line
+	//	is in the right format                
+	if (element.closest("tr").is(":last-child")) {
+	    var tr = '<tr>' +
+	        '<td>+</td>' +
+	        '<td class="editable"></td>' +
+	        '</tr>';
+	    $('#xp_steps tbody').append(tr);
+	    $('#xp_steps tr:last .editable')
+	    	.editable(editableOptions)
+	    	.on('save', stepSave)
+	    	.on('hidden', stepNext);
+	}
+	// set value and add step number
+	element.text(params.newValue);
+	element.removeClass('draft');
+	element.prev().text($('#xp_steps tr').length - 3);
+}
+
+function defineXPDone() {
+	xpStepsArray.length = 0;
+	xpName = $('#xp_flow_name').text();
+	// save all steps
+	$('#xp_steps tbody tr').not(':last').each(function(){
+		var step = $(this).find('td:last').text();
+		xpStepsArray.push(step);
+	});
+	console.log('steps: '+JSON.stringify(xpStepsArray));
+	// lock table editing and remove buttons
+	$('#xp_flow_name').editable('disable');
+	$('#xp_steps .editable').editable('disable');
+	$('#xp_create').addClass('disabled');
+}
 
 function searchXP() {
     //reportString = 'Type: SAVED_TESTS\n';
@@ -78,10 +114,16 @@ function searchXP() {
 function clearXP() {
 	var tr = '<tr>' +
 	    '<td>+</td>' +
-	    '<td class="quickedit draft">Click to edit</td>' +
+	    '<td class="editable"></td>' +
 	    '</tr>';
 	$('#xp_steps tbody').empty().append(tr);
-	$('#xp_flow_name').text('Click to edit name');
-	$('#xp_flow_name').addClass('draft');
-    $('#xp_steps .quickedit').quickEdit(quickEditConfig);
+	$('#xp_flow_name').text('');
+	$('#xp_flow_name').editable('setValue', '', false);
+	$('#xp_flow_name').editable('enable');
+	$('#xp_create').removeClass('disabled');
+
+    $('#xp_steps .editable')
+    	.editable(editableOptions)
+    	.on('save', stepSave)
+	    .on('hidden', stepNext);
 }
