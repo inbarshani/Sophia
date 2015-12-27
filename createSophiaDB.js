12eff3a1726da37ed69f2b6fa594bd56c1d8117e
Inbar Shani <inbar.shani@hp.com>
2015-12-27 14:35:59 +0200

    init DB in docker host, then share volumes with relevant processes

--- createSophiaDB.js
+++ createSophiaDB.js
@@ -1,3 +1,16 @@
-var tests_queries = require('./lib/tests_queries');
+var sophia_config = require('./lib/sophia_config');
+var mkpath = require('mkpath');
 
-tests_queries.importScheme();
+// make sure the DB folder exists
+mkpath(require('path').dirname(sophia_config.TESTS_DB_FILE), 
+	function(err){
+		if (!err)
+		{
+			var tests_queries = require('./lib/tests_queries');
+			tests_queries.importScheme();
+		}
+        else
+            console.log('ERROR: failed to create TESTS DB at '+
+                sophia_config.TESTS_DB_FILE+':\n'+
+                err);
+	});
